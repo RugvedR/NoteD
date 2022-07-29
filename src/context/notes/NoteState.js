31ebd361b0422d469e30cbb1567f4bd2b32ae41f
RugvedR <rugvedr9@gmail.com>
2022-07-29 08:55:27 +0530

    context api introduced

---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import NoteContext from "./noteContext";
+
+const NoteState = (props) => {
+
+    const s1 = {
+        "name": "rugved",
+        "class": "9a"
+    }
+
+    const [state, setState] = useState(s1);
+    const update = ()=>{
+        setTimeout(() => {
+            setState({
+                "name": "Larry",
+                "class": "5b"
+            })
+        }, 1000);
+    }
+
+    return (
+        <NoteContext.Provider value={{state, update}} >
+            {props.children}
+        </NoteContext.Provider>
+    )
+}
+
+
+
+export default NoteState;
