63908be600d0243a7fb7a26f303f9c35e8b129a7
RugvedR <rugvedr9@gmail.com>
2022-07-29 12:33:56 +0530

    Note and Noteitem components added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,7 @@
 import React, { useContext } from 'react'
-import noteContext from '../context/notes/noteContext';
+import Notes from './Notes';
 
 const Home = () => {
-  const context = useContext(noteContext);
-  const {notes, setNotes} = context;
 
   return (
     <div>
@@ -26,17 +24,7 @@ const Home = () => {
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
-      <div className="container my-3">
-        <h2>Your Notes</h2>
-        {notes.map((note)=>{
-            return (
-              <div>
-                <h5>{note.title}</h5>
-                <p>{note.description} </p>
-              </div>
-            )
-        })}
-      </div>
+      <Notes/>
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -23,6 +23,24 @@ const NoteState = (props) => {
             "__v": 0
         },
         {
+          "_id": "62e23e28d9165d26d79e3218",
+          "user": "62e23ce8d9165d26d79e3214",
+          "title": "lookout",
+          "description": "lookout for the new image that you have there",
+          "tag": "personal",
+          "date": "2022-07-28T07:43:36.796Z",
+          "__v": 0
+        },
+        {
+          "_id": "62e23e28d9165d26d79e3218",
+          "user": "62e23ce8d9165d26d79e3214",
+          "title": "New pic",
+          "description": "delete this new pic here",
+          "tag": "personal",
+          "date": "2022-07-28T07:43:36.796Z",
+          "__v": 0
+        },  
+        {
           "_id": "62e2b175626251507f7c4196",
           "user": "62e23ce8d9165d26d79e3214",
           "title": "very important note",
