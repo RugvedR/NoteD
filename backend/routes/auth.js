d34f4c51882a53a1139aa059c661654647551660
RugvedR <rugvedr9@gmail.com>
2022-07-31 09:40:00 +0530

    login page added with redirection to home

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -65,6 +65,7 @@ router.post('/login',[
 
 ], async (req, res)=>{
 
+    let success = false;
     // if there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -80,7 +81,8 @@ router.post('/login',[
 
         const passwordCompare = await bcrypt.compare(password, user.password);
         if(!passwordCompare){
-            return res.status(400).json({error: "Please try to login with correct credentials"});
+            success = false;
+            return res.status(400).json({success, error: "Please try to login with correct credentials"});
         }
 
         const data = {
@@ -89,7 +91,8 @@ router.post('/login',[
             }
         }
         const authtoken = jwt.sign(data, JWT_SECRET);
-        res.json({authtoken});
+        success = true;
+        res.json({success, authtoken});
 
     } catch(error){
         console.error(error.message);
